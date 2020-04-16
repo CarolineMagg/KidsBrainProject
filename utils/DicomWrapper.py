cd73d13af10c80296eb48a1d5dec419346f439c6
CarolineMagg <caroline_magg@hotmail.com>
2020-04-16 08:09:12 +0200

    Add patient data wrapper
    * dicom data wrapper
    * patient data wrapper to read several dicom files of one patient
    * usage and first testing of segmentation methods

---
+++ utils/DicomWrapper.py
@@ -0,0 +1,96 @@
+########################################################################################################################
+# Class to wrap Dicom Data into customized data type
+########################################################################################################################
+import os
+import pydicom
+import cv2
+from natsort import natsorted
+
+__author__ = "c.magg"
+
+
+class DicomWrapper:
+
+    def __init__(self, dicom_path, frame=True, debug=True):
+        self._debug = debug
+        self._frame = frame
+        self._path_dicom = dicom_path
+
+        self._imgs = None
+        self._ref_dcm = None
+        self._dimensions = None
+        self._pixel_spacing = None
+        self._position = None
+        self._correction = None
+        self._slice_location = None
+        self._modality = None
+
+        self._read_dicom()
+        if self._frame:
+            self._read_reference_frame_info()
+
+    def get_correction(self):
+        if self._correction is not None:
+            return self._correction
+        else:
+            raise ValueError("Correction is not set.")
+
+    def get_images(self, ind=None):
+        if ind is None:
+            return [cv2.normalize(x, x, 0, 255, 32) for x in self._imgs]
+        else:
+            x = self._imgs[ind]
+            return cv2.normalize(x, x, 0, 255, 32)
+
+    def get_slice_location(self, ind):
+        if self._slice_location is not None:
+            if ind is None:
+                return self._slice_location
+            else:
+                return self._slice_location[ind]
+        else:
+            raise ValueError("Slice location is not set.")
+
+    def _read_dicom(self):
+        """
+        Method to read dicom and convert to list of numpy arrays
+        :return:
+        """
+        files = natsorted(os.listdir(self._path_dicom))
+        try:
+            dcms_list = [pydicom.read_file(os.path.join(self._path_dicom, fn)) for fn in files]
+            self._imgs = [dcm.pixel_array for dcm in dcms_list]
+            self._slice_location = [float(x.get('SliceLocation', '-100')) for x in dcms_list]
+            self._ref_dcm = dcms_list[0]
+        except:
+            raise ImportError("Dicom-file couldn't be opened.")
+
+    def _read_reference_frame_info(self):
+        """
+        Method to read reference frame information and determine:
+        * dicom dimensions, eg. 512x512x200
+        * pixel spacing, eg. 0.978,0.978,1.0
+        * position, eg. -250,-250,388
+        * correction (mapping from reference coordinate sytem to input pixel space), eg. -250,-217,0
+        :return:
+        """
+
+        try:
+            self._dimensions = (int(self._ref_dcm.Rows), int(self._ref_dcm.Columns), len(self._imgs))
+            self._pixel_spacing = (float(self._ref_dcm.PixelSpacing[0]), float(self._ref_dcm.PixelSpacing[1]),
+                                   float(self._ref_dcm.SliceThickness))
+            self._position = tuple((float(p) for p in self._ref_dcm.ImagePositionPatient))
+            self._correction = [abs(0 + self._position[0]) + 1, abs(0 + self._position[1]) + 1, 0]
+            self._modality = self._ref_dcm.Modality
+        except:
+            raise ValueError("Reference frame information could not be processed.")
+
+        if self._debug:
+            print("Image size.......:", self._dimensions)
+            print("Pixel spacing....:", self._pixel_spacing)
+            print("Slices number....:", len(self._slice_location))
+            print("Slices location...:", self._slice_location[0], " - ", self._slice_location[-1])
+            print("Position.........:", self._position)
+            print("Resulting correction:", self._correction)
+
+
--- utils/PatientData.py
+++ utils/PatientData.py
@@ -4,48 +4,57 @@
 import pandas as pd
 import pydicom
 import numpy as np
-import os
+import matplotlib.pyplot as plt
+import cv2
+import os.path
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from utils.DicomWrapper import DicomWrapper
 
 __author__ = "c.magg"
 
 
 class PatientData:
 
-    def __init__(self, preop_dicom_path, contours_dicom_path, debug=True):
+    def __init__(self, pre_dicom_path, post_dicom_path, contours_dicom_path, debug=True):
         self._debug = debug
-        self._preop_dicom_path = preop_dicom_path
+        self._preop_dicom_path = pre_dicom_path
+        self._postop_dicom_path = post_dicom_path
         self._contour_dicom_path = contours_dicom_path
-        self._preop_imgs = None
-        self._preop_imgs_location = None
+        self._preop_dicoms = None
+        self._correction = None
+        self._postop_dicoms = []
         self._contour_dcm = None
         self.contour_list_names = pd.DataFrame(columns=['ID', 'RoiNumber', 'RoiName'])
         self.contour_list_names_filtered = pd.DataFrame(columns=['ID', 'RoiNumber', 'RoiName'])
         self.contour_dict_points_layerwise = {}
-        self._read_dicom_preop()
+        self._read_dicom()
         self._read_dicom_contour()
         self._read_contour_list()
 
-    def _read_dicom_preop(self):
+    def _read_dicom(self):
         """
-        Method to read dicom preop (CT) data and pixel array of dicoms
+        Method to create DicomWrapper objects for pre and post treatment dicoms
+        pre-treatment dicom is used as reference for all the other dicom files (correction factor)
         :return:
         """
-        files = os.listdir(self._preop_dicom_path)
-        try:
-            preop_dcms_list = [pydicom.read_file(os.path.join(self._preop_dicom_path, fn)) for fn in files]
-            self._preop_imgs = [dcm.pixel_array for dcm in preop_dcms_list]
-            self._preop_imgs_location = [float(x.get('SliceLocation', '-100')) for x in preop_dcms_list]
-        except:
-            raise ImportError("Dicom-file preop couldn't be opened.")
-        if self._debug:
-            for i in range(len(self._preop_imgs_location)):
-                print("Slice location...:", self._preop_imgs_location[i])
-            ds = preop_dcms_list[0]
-            print("Image size.......: {rows:d} x {cols:d}, {size:d} bytes".format(
-                rows=int(ds.Rows), cols=int(ds.Columns), size=len(ds.PixelData)))
-            if 'PixelSpacing' in ds:
-                print("Pixel spacing....:", ds.PixelSpacing)
-            print("Slices number....:", len(self._preop_imgs_location))
+
+        print("\nPreop dicom")
+        self._preop_dicoms = DicomWrapper(self._preop_dicom_path, True, True)
+        self._correction = self._preop_dicoms.get_correction()
+        for postop_path in self._postop_dicom_path:
+            print("\nPostop", postop_path)
+            self._postop_dicoms.append(DicomWrapper(postop_path, True, True))
+
+    def get_pre_images(self, ind=None):
+        return self._preop_dicoms.get_images(ind)
+
+    def get_post_images(self, ind=None):
+        return [x.get_images(ind) for x in self._postop_dicoms]
+
+    def get_slice_location(self, ind=None):
+        return self._preop_dicoms.get_slice_location(ind)
 
     def _read_dicom_contour(self):
         """
@@ -88,15 +97,17 @@ class PatientData:
 
         if type(roiname) == str:
             roiname = [roiname]
-        if mode == 'approx':
-            self.contour_list_names_filtered = pd.DataFrame(
-                [x for y in roiname for idx, x in self.contour_list_names.iterrows() if y in x['RoiName']])
-        if mode == 'exact':
-            self.contour_list_names_filtered = pd.DataFrame(
-                [x for y in roiname for idx, x in self.contour_list_names.iterrows() if y == x['RoiName']])
+        if roiname is not None:
+            if mode == 'approx':
+                self.contour_list_names_filtered = pd.DataFrame(
+                    [x for y in roiname for idx, x in self.contour_list_names.iterrows() if y in x['RoiName']])
+            if mode == 'exact':
+                self.contour_list_names_filtered = pd.DataFrame(
+                    [x for y in roiname for idx, x in self.contour_list_names.iterrows() if y == x['RoiName']])
 
-        self.contour_list_names_filtered = self.contour_list_names_filtered.append(pd.DataFrame(
-            [x for y in roinumber for idx, x in self.contour_list_names.iterrows() if y == x['RoiNumber']]))
+        if roinumber is not None:
+            self.contour_list_names_filtered = self.contour_list_names_filtered.append(pd.DataFrame(
+                [x for y in roinumber for idx, x in self.contour_list_names.iterrows() if y == x['RoiNumber']]))
 
         if len(self.contour_list_names_filtered) != 0:
             self.contour_list_names_filtered = self.contour_list_names_filtered.drop_duplicates()
@@ -120,9 +131,75 @@ class PatientData:
             for j in range(self._contour_dcm.ROIContourSequence.__getitem__(i).ContourSequence.__len__()):
                 contour = np.array(
                     self._contour_dcm.ROIContourSequence.__getitem__(i).ContourSequence.__getitem__(j).ContourData)
-                contour = contour.reshape(int(contour.size / 3.), 3)
+                contour = contour.reshape(int(contour.size / 3.), 3) + self._correction
                 contour_points_layerwise.append(contour)
             contours_dict_point_layerwise[name] = contour_points_layerwise
 
         self.contour_dict_points_layerwise = contours_dict_point_layerwise
-        return self.contour_dict_points_layerwise
+
+    def show_overlay_pre(self, ind, struct):
+        """
+        Method to show the overlay of pre-treatment image and contour
+        :param ind: slice index
+        :param struct: name of roi of interest
+        :return:
+        """
+        img_preop = self.get_pre_images(ind)
+        contour_img = self.create_contour_overlay(ind, struct)
+        overlay_pre = cv2.addWeighted(img_preop.astype(np.uint16), 1.0, contour_img, 0.8, 0)
+        plt.imshow(overlay_pre)
+        plt.show()
+
+    def show_overlay_post(self, ind, struct):
+        """
+        Method to show the overlay of post-treatment image and contour
+        :param ind: slice index
+        :param struct: name of roi of interest
+        :return:
+        """
+        img_postop = self.get_post_images(ind)
+        contour_img = self.create_contour_overlay(ind, struct)
+        for img in img_postop:
+            overlay_post = cv2.addWeighted(img.astype(np.uint16), 1.0, contour_img, 0.8, 0)
+            plt.imshow(overlay_post)
+            plt.show()
+
+    def get_contour(self, ind, struct):
+        """
+        Method to get contour coordinates of given structure and slice
+        :param ind: slice index
+        :param struct: name of roi of interest
+        :return:
+        """
+        if struct not in self.contour_dict_points_layerwise.keys():
+            raise ValueError("ROIName was not filtered.")
+        loc = self.get_slice_location(ind)
+        vertices = np.array([x[:,0:2] for x in self.contour_dict_points_layerwise[struct] if x[0][2] == loc])
+        if len(vertices) != 0:
+            vertices = vertices[0].astype(np.int32)
+        return vertices
+
+    def create_contour_overlay(self, ind, struct):
+        """
+        Method to create the contour 2D mask of slice ind and given roi name
+        :param ind: slice index
+        :param struct: name of roi of interest
+        :return:
+        """
+        contour_img = np.zeros_like(self.get_pre_images(ind))
+        vertices = self.get_contour(ind, struct)
+        if len(vertices) != 0:
+            cv2.drawContours(contour_img, [vertices], -1, (255, 255, 255), -1)
+        else:
+            print('No contour')
+        return contour_img
+
+    def show_overlays(self, ind, struct):
+        """
+        Method to show overlays for pre- and post-treatment data
+        :param ind: slice index
+        :param struct: name of roi of interest
+        :return:
+        """
+        self.show_overlay_pre(ind, struct)
+        self.show_overlay_post(ind, struct)
